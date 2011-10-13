2135c23320af5b5ccb71fb1df276be21765a648b
codereflection <jeff@codingreflection.com>
2011-10-13 13:48:47 -0700

    Filling the trunk, tree, ground, and sky.. text not working through, messes with the ground fill

--- src/bobross.js
+++ src/bobross.js
@@ -13,27 +13,57 @@ var bobross = (function(){
             var context = canvas.getContext("2d");
             var center = canvas.width / 2;
 
-            // background
-            context.fillStyle = "#aaaaaa";
-            context.fillRect(0,0,500,300);
-            context.font = "20px _sans";
-
-            // border
-            context.strokeStyle = "#000000";
-            context.strokeRect(5, 5, 490, 290);
-
-            // text
-            context.fillStyle = "#000000";
+/*
+            // text -- commented out because this is affecting the ground for some reason.. :/
+            context.font = "30px Verdana";
+            context.lineWidth = 1;
+            context.strokeStyle = "e3e3e3";
+            context.fillStyle = "#e3e3e3";
+            context.fill();
             context.textAlign = "center";
-            context.fillText ("Happy Little Canvas", canvas.width / 2, 30);
+            context.fillText("Happy Little Canvas", canvas.width / 2, 40);
+*/
+
+
+            // background
+            var backGrad = context.createLinearGradient(0, 0, 0, 300);
+            backGrad.addColorStop(0, "#4A6491");
+            backGrad.addColorStop(1, "#D0E4F2");
+            context.fillStyle = backGrad;
+            context.fillRect(0,0,500,300);
+
+
+            // ground
+            context.lineWidth = 1;
+            context.beginPath();
+            context.moveTo(1, 260);
+            context.bezierCurveTo(20, 250, 50, 280, center, 250);
+            context.bezierCurveTo(center + 50, 250, center + 50, 240, canvas.width-1, 280);
+            context.lineTo(canvas.width-1, canvas.height-1);
+            context.lineTo(1, canvas.height-1);
+            context.lineTo(1, 260);
+            context.closePath();
+            var groundGrad = context.createLinearGradient(0, 200, 0, canvas.height);
+            groundGrad.addColorStop(0, "#232617");
+            groundGrad.addColorStop(0.5, "#42350B");
+            groundGrad.addColorStop(1, "#63661A");
+            context.fillStyle = groundGrad;
+            context.fill();
+            context.strokeStyle = "#42350B";
+            context.stroke();
+
 
             // trunk
             context.strokeStyle = "Brown";
-            context.lineWidth = 20;
+            context.lineWidth = 1;
             context.lineCap = "square";
             context.beginPath();
-            context.moveTo(center, 285);
-            context.lineTo(center, 280);
+            context.rect(center - 10, 260, 10, 25);
+            var trunkGrad = context.createLinearGradient(center - 10, 0, center + 10, 0);
+            trunkGrad.addColorStop(0, "#241A0C");
+            trunkGrad.addColorStop(1, "#D6820D");
+            context.fillStyle = trunkGrad;
+            context.fill();
             context.stroke();
             context.closePath();
 
@@ -62,8 +92,20 @@ var bobross = (function(){
             context.lineTo(center + 60, 230);
             context.lineTo(center + 100, 270);
             context.lineTo(center, 270);
+
+            // tree fill
+            var branchGrad = context.createLinearGradient(center - 100, 150, center + 100, 50);
+            branchGrad.addColorStop(0, "#466100");
+            branchGrad.addColorStop(1, "#89B41C");
+            context.fillStyle = branchGrad;
+            context.fill();
             context.stroke();
             context.closePath();
+
+            
+            // border
+            context.strokeStyle = "#000000";
+            context.strokeRect(5, 5, 490, 290);
         },
         sayHello : function(){
             return "Let's beat the devil out of it!";
